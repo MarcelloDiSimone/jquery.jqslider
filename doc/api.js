66b6b7fcee6e9dd4f7877911404b836de8a052f0
MarcelloDiSimone <marcello.disimone@innogames.de>
2012-12-23 23:22:43 +0100

    Replaced the API documentation with a new one, generated with yuidoc

---
+++ doc/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "JQSlider"
+    ],
+    "modules": [
+        "JQSlider"
+    ],
+    "allModules": [
+        {
+            "displayName": "JQSlider",
+            "name": "JQSlider",
+            "description": "A responsive and fast content Slider\njQSlider follows a new approach in building a slider. Where most slider-plugins are moving the whole list of slides\nwhen animating, jQSlider animates only the two slides necessary for the animation simultaneously. This is not only an\nway to optimize the performance of the animation, it also allows to realize a 100% css based scaling which leads us to\na full responsive slider."
+        }
+    ]
+} };
+});
